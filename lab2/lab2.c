b415d7eea68c5e03cff21e2bd6ecb9494c89cb9b
hkctkuy <hkctkuy@gamil.com>
2021-10-05 10:27:19 +0300

    Rename variables in lab2.c

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -4,45 +4,45 @@
 
 int main() {
 
-    float **matrix1, **matrix2, **matrix3, scalar; int n1, m1, n2, m2; char charcacter;
+    float **matrix1, **matrix2, **matrix3, scalar; int row_size_1, column_size_1, row_size_2, column_size_2; char charcacter;
 
     printf("%s", "Input size of your matrix (num_of_col num_of_str): ");
 
-    scanf("%d%d%c", &n1, &m1, &charcacter);
+    scanf("%d%d%c", &row_size_1, &column_size_1, &charcacter);
 
-    matrix1 = get_matrix(n1, m1);
+    matrix1 = get_matrix(row_size_1, column_size_1);
 
     if (matrix1 != 0) {
 
-        print_matrix(matrix1, n1, m1);
+        print_matrix(matrix1, row_size_1, column_size_1);
     }
 
     printf("%s", "Input scalar: ");
 
     scanf("%f", &scalar);
 
-    matrix_scalar_multiplication(matrix1, n1, m1, scalar);
+    matrix_scalar_multiplication(matrix1, row_size_1, column_size_1, scalar);
 
     if (matrix1 != 0) {
 
-        print_matrix(matrix1, n1, m1);
+        print_matrix(matrix1, row_size_1, column_size_1);
     }
     printf("%s", "Input size of your matrix (num_of_col num_of_str): ");
 
-    scanf("%d%d%c", &n2, &m2, &charcacter);
+    scanf("%d%d%c", &row_size_2, &column_size_2, &charcacter);
 
-    matrix2 = get_matrix(n2, m2);
+    matrix2 = get_matrix(row_size_2, column_size_2);
 
-    matrix3 = matrix_multiplication(matrix1, n1, m1, matrix2, n2, m2);
+    matrix3 = matrix_multiplication(matrix1, row_size_1, column_size_1, matrix2, row_size_2, column_size_2);
 
     if (matrix3 != 0) {
 
-        print_matrix(matrix3, n1, m2);
+        print_matrix(matrix3, row_size_1, column_size_2);
     }
 
-    printf("%f\n", det(matrix1, n1, m1));
+    printf("%f\n", det(matrix1, row_size_1, column_size_1));
 
-    print_matrix(matrix1, n1, m1);
+    print_matrix(matrix1, row_size_1, column_size_1);
 
     return 0;
 }
